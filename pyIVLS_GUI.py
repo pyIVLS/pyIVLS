d424e933f3df4881488a68be968d6d4be1e46459
Otso Häkkänen <otso.hakkanen@aalto.fi>
2026-02-03 10:59:16 +0200

    Persistent manipulator GUI state in touchdetect on initialization

--- pyIVLS_GUI.py
+++ pyIVLS_GUI.py
@@ -41,7 +41,7 @@ class pyIVLS_GUI(QObject):
     ############################### Slots
     @pyqtSlot(str)
     def show_message(self, str):
-        msg = QtWidgets.QMessageBox()
+        msg = QtWidgets.QMessageBox()  # cannot use parent = self.window, because the message simply does not work then ????
         msg.setText(str)
         msg.setWindowTitle("Warning")
         msg.setIcon(QtWidgets.QMessageBox.Icon.Warning)
