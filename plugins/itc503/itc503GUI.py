79b8b1b28f886eb3477cab18d1cb08558871652b
lumipallero <tuokkolalumi@gmail.com>
2026-04-08 11:17:12 +0300

    itc503 plugin updated for temperature monitoring from the ITC

--- plugins/itc503/itc503GUI.py
+++ plugins/itc503/itc503GUI.py
@@ -22,9 +22,12 @@ from datetime import datetime, timedelta
 import matplotlib.dates as mdates
 from PyQt6 import uic
 from PyQt6.QtCore import QObject, QTimer, pyqtSignal
-
+from PyQt6.QtWidgets import QVBoxLayout, QFileDialog
 from itc503 import itc503
 from MplCanvas import MplCanvas  # this should be moved to some pluginsShare
+from worker_thread import WorkerThread
+from threadStopped import ThreadStopped, thread_with_exception
+import numpy as np
 
 # from mock import itc503  # for testing without the real device
 import time
@@ -45,7 +48,9 @@ class itc503GUI(QObject):
     log_message = pyqtSignal(str)
     info_message = pyqtSignal(str)
     closeLock = pyqtSignal(bool)
-
+    arrayT = []
+    arraytemp = []
+    runningFlag = False
     ########Functions
     def __init__(self):
         super(itc503GUI, self).__init__()
@@ -59,8 +64,8 @@ class itc503GUI(QObject):
         self.itc503 = itc503()
 
         self._connect_signals()
+        self.settings = {}
         self._create_plt()
-
         # Set a timer for the temperature display
         self.timer = QTimer()
         self.timer.timeout.connect(self._update_display)
@@ -69,18 +74,24 @@ class itc503GUI(QObject):
         self.settingsWidget.connectButton.clicked.connect(self._connectAction)
         self.settingsWidget.disconnectButton.clicked.connect(self._disconnectAction)
         self.settingsWidget.setTButton.clicked.connect(self._setTAction)
+        self.settingsWidget.directoryButton.clicked.connect(self._getAddress)
         self.settingsWidget.periodCheck.clicked.connect(self._displayAction)
+        self.settingsWidget.saveButton.clicked.connect(self._createFile)
+
 
     def _create_plt(self):
         self.sc = MplCanvas(self, width=5, height=4, dpi=100)
         self.axes = self.sc.fig.add_subplot(111)
-        self.axes.set_xlabel("time (HH:MM)")
-
-        degree_sign = "\N{DEGREE SIGN}"
-        self.axes.set_ylabel(f"Temperature ({degree_sign}C)")
-
-        self.MDIWidget.displayLayout.addWidget(self.sc._create_toolbar(self.MDIWidget))
-        self.MDIWidget.displayLayout.addWidget(self.sc)
+        self.axes.set_xlabel("time (s)")
+        self.axes.set_ylabel("Temperature (K)")
+        self.axes.set_ylim(0, 350) 
+        
+        #self.axes.text(0.9, 0.1, f'{self.arraytemp[-1]}', dict(size=20))
+        layout = QVBoxLayout()
+        layout.addWidget(self.sc._create_toolbar(self.MDIWidget))
+        layout.addWidget(self.sc)
+        self.MDIWidget.setLayout(layout)
+        #self._GUIchange_deviceConnected(True)
 
     ########Functions
     ########GUI Slots
@@ -129,8 +140,9 @@ class itc503GUI(QObject):
             return [status, info]
         return [0, "OK"]
 
+
     def _displayAction(self):
-        if self.timer.isActive():
+        """if self.timer.isActive():
             self.timer.stop()
             self._GUIchange_display(False)
         else:
@@ -144,7 +156,21 @@ class itc503GUI(QObject):
                 self.display_data = ""
                 self._update_display()
                 self.timer.start(self.settings["period"] * 1000)
-                self._GUIchange_display(True)
+                self._GUIchange_display(True)"""
+        [status, info] = self._parse_settings_display()
+        if self.timer.isActive():
+            self.timer.stop()
+            return [1, "Running already"]
+        if status:
+            self.log_message.emit(datetime.now().strftime("%H:%M:%S.%f") + f" : itc503 : {info}, status = {status}")
+            self.info_message.emit(f"itc503 plugin : {info}")
+        else:
+            self.run_thread = thread_with_exception(self._run_check)
+            self.run_thread.start()
+            self.runningFlag = True
+            
+            
+            #self._GUIchange_display(True)
 
     ########Functions
     ################################### internal
@@ -164,6 +190,15 @@ class itc503GUI(QObject):
                 {"Error message": "Value error: set temperature field should be numeric"},
             ]
 
+        return [0, self.settings]
+
+    def _parse_settings_save(self):
+        self.settings["filename"] = self.settingsWidget.fileNameLine.text()
+        self.settings["comment"] = self.settingsWidget.commentLine.text()
+        self.settings["samplename"] = self.settingsWidget.sampleNameLine.text()
+        self.settings["address"] = self.settingsWidget.addressLine.text()
+        self.settings["log"] = self.settingsWidget.logCheckBox.isChecked()
+        self.settings["wholetime"] = self.settingsWidget.logWholeBox.isChecked()
         return [0, self.settings]
 
     def _parse_settings_display(self):
@@ -195,49 +230,64 @@ class itc503GUI(QObject):
 
         return [0, self.settings]
 
-    def _update_display(self):
-        try:
-            info = self.itc503.getData()
-        except Exception as e:
-            self.log_message.emit(datetime.now().strftime("%H:%M:%S.%f") + f" : itc503 plugin : {e}, status = 4")
-            self.info_message.emit(f"itc503 plugin : {e}")
-            self.timer.stop()
-            self._GUIchange_display(False)
-            return [e, {"Error message": f"{e}"}]
 
-        timeNow = datetime.now()
-        if self.display_data.count("\n") == self.settings["periodpts"]:
-            self.display_data = self.display_data[self.display_data.find("\n") + 1 :]
-        self.display_data = self.display_data + timeNow.strftime("%H:%M:%S.%f") + f": {info}K\n"
-        self.MDIWidget.outputEdit.clear()
-        self.MDIWidget.outputEdit.append(self.display_data)
-        temperature = info
-        if not self.Xdata:
-            self.axes.cla()
-            self.Xdata = [timeNow]
-            self.Ydata = [temperature]
-            plot_refs = self.axes.plot(self.Xdata, self.Ydata, "bo")
-            self.axes.set_xlabel("time (HH:MM)")
-            degree_sign = "\N{DEGREE SIGN}"
-            self.axes.set_ylabel(f"Temperature ({degree_sign}C)")
-            self._plot_temperature = plot_refs[0]
-            self.axes.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-            self.axes.set_xlim(
-                self.Xdata[-1] - timedelta(seconds=self.settings["period"]),
-                self.Xdata[-1] + timedelta(seconds=self.settings["period"]) * self.settings["periodpts"],
-            )
-        else:
-            self.Xdata.append(timeNow)
-            self.Ydata.append(temperature)
-            self._plot_temperature.set_xdata(self.Xdata)
-            self._plot_temperature.set_ydata(self.Ydata)
-            if len(self.Xdata) > self.settings["periodpts"]:
-                self.axes.set_xlim(
-                    self.Xdata[-1] - timedelta(seconds=self.settings["period"] * self.settings["periodpts"]),
-                    self.Xdata[-1] + timedelta(seconds=self.settings["period"]),
-                )
-        self.axes.set_ylim(min(self.Ydata) - 10, max(self.Ydata) + 10)  # +/- 10 just a random margin for plotting
+
+
+
+    def _run_check(self):
+        timeNow = time.time()
+        #timeDelta = 0  add in the future
+        points = int(self.settings["periodpts"]/self.settings["period"])
+        for i in range(points):
+            self.arraytemp.append(self.itc503.getData())
+            self.arrayT.append(time.time() - timeNow)
+            self._render_mdi()
+            time.sleep(self.settings["period"])
+        self.runningFlag = False
+        self._createFileLoop()
+        self.run_thread.thread_stop()
+    
+    def _run_check_long(self):
+        timeNow = time.time()
+        #timeDelta = 0  add in the future
+        points = int(3600/self.settings["period"])
+        for i in range(points):
+            self.arraytemp.append(self.itc503.getData())
+            self.arrayT.append(time.time() - timeNow)
+            self._render_mdi()
+            time.sleep(self.settings["period"])
+        self.runningFlag = False
+        self._createFileLoop()
+        self.run_thread.thread_stop()
+
+    
+
+    def _render_mdi(self):
+        xmin, xmax = self.axes.get_xlim()
+        ymin, ymax = self.axes.get_ylim()
+        xmax = max(self.arrayT)
+        self.axes.cla()
+        self.axes.set_xlabel("time (HH:MM)")
+        self.axes.set_ylabel("Temperature (K)")
+        self.axes.plot(self.arrayT, self.arraytemp, "o")
+        self.axes.set_xlim(xmin, xmax)
+        self.axes.set_ylim(ymin, ymax)
+        self.axes.set_title(f'T = {self.arraytemp[-1]} K')
         self.sc.draw()
+        return [0]
+
+
+    def _update_display(self):
+        xmin, xmax = self.axes.get_xlim()
+        ymin, ymax = self.axes.get_ylim()
+        self.axes.cla()
+        self.axes.set_xlabel("Wavelength (nm)")
+        self.axes.set_ylabel("Intensity (calib. arb. un.)")
+        self.axes.plot(self.arrayT, self.arraytemp, "b-")
+        self.axes.set_xlim(xmin, xmax)
+        self.axes.set_ylim(ymin, ymax)
+        self.sc.draw()
+        return [0]
 
     def _setT(self):
         try:
@@ -245,6 +295,99 @@ class itc503GUI(QObject):
             return [0, "OK"]
         except Exception as e:
             return [4, {"Error message": f"{e}"}]
+
+    ####Functions to do with saving
+    def _getAddress(self):
+        address = self.settingsWidget.addressLine.text()
+        if not (os.path.exists(address)):
+            address = self.path
+        address = QFileDialog.getExistingDirectory(
+            None,
+            "Select directory for saving",
+            address,
+            options=QFileDialog.Option.ShowDirsOnly | QFileDialog.Option.DontResolveSymlinks,
+        )
+        if address:
+            self.settingsWidget.addressLine.setText(address)
+
+
+    def _createFile(self):
+        fileheader = self._itcMakeHeader()
+
+        saveAddress = self.settingsWidget.addressLine.text() + os.path.sep + self.settingsWidget.fileNameLine.text()+".txt"
+        np.savetxt(
+            saveAddress,
+            list(zip(self.arrayT, self.arraytemp)),
+            fmt="%.9e",
+            delimiter=";",
+            newline="\n",
+            header=fileheader,
+            footer="#[EndOfFile]",
+            comments="#",
+        )
+    def _createFileLoop(self):
+        fileheader = self._itcMakeHeader()
+
+        saveAddress = self.settings["address"] + os.path.sep + self.settings["filename"] + str(self.arrayT[1]) + "K" + ".txt"
+        np.savetxt(
+            saveAddress,
+            list(zip(self.arrayT, self.arraytemp)),
+            fmt="%.9e",
+            delimiter=";",
+            newline="\n",
+            header=fileheader,
+            footer="#[EndOfFile]",
+            comments="#",
+        )
+        
+
+    def _itcMakeHeader(self):
+        ###following the structure of files generated by Thorlabs software
+        ### a part of values are just const, they may be replaces with real values
+        #
+        # structure of the varDict
+        #
+        # varDict['average'] - int:averaging
+        # varDict['integrationtime'] - float:integration time in seconds
+        # varDict['triggermode'] - external trigger = 1 / internal = 0
+        # varDict['name'] - str:sample name
+        # varDict['comment'] - str:comment
+        comment = "ITC403 operated by pyIVSL\n"
+        comment = f"{comment}#[SpectrumHeader]\n"
+        comment = f"{comment}Date;{datetime.now().strftime('%Y%m%d')}\n"
+        comment = f"{comment}Time;{datetime.now().strftime('%H%M%S%f')[:-4]}\n"
+        comment = f"{comment}GMTTime;{datetime.utcnow().strftime('%H%M%S%f')[:-4]}\n"
+        comment = f"{comment}XAxisUnit;time(s)\n"
+        comment = f"{comment}YAxisUnit;temperature(K)\n"
+        comment = f"{comment}Average;0\n"
+        comment = f"{comment}RollingAverage;0\n"
+        comment = f"{comment}SpectrumSmooth;0\n"
+        comment = f"{comment}SSmoothParam1;0\n"
+        comment = f"{comment}SSmoothParam2;0\n"
+        comment = f"{comment}SSmoothParam3;0\n"
+        comment = f"{comment}SSmoothParam4;0\n"
+        comment = f"{comment}IntegrationTime;0\n"
+        comment = f"{comment}TriggerMode;0\n"
+        comment = f"{comment}InterferometerSerial;M00903839\n"
+        comment = f"{comment}Source\n"
+        comment = f"{comment}AirMeasureOpt;0\n"
+        comment = f"{comment}WnrMin;0\n"
+        comment = f"{comment}WnrMax;0\n"
+        comment = f"{comment}Length;3648\n"
+        comment = f"{comment}Resolution;0\n"
+        comment = f"{comment}ADC;0\n"
+        comment = f"{comment}Instrument;0\n"
+        comment = f"{comment}Model;itc503 smart \n"
+        comment = f"{comment}Type;emission\n"
+        comment = f"{comment}AirTemp;0\n"
+        comment = f"{comment}AirPressure;0\n"
+        comment = f"{comment}AirRelHum;0\n"
+        comment = f"{comment}Name;{self.settings['samplename']}\n"
+        comment = f"{comment}Comment;{self.settings['comment']}\n"
+        comment = f"{comment}#[Data]\n"
+        return comment
+
+
 
     ########Functions
     ###############GUI setting up
@@ -270,6 +413,13 @@ class itc503GUI(QObject):
         self.settingsWidget.sweepEndEdit.setText(plugin_info["sweepend"])
         self.settingsWidget.sweepPtsEdit.setText(plugin_info["sweeppts"])
         self.settingsWidget.sweepStabilizationEdit.setText(plugin_info["sweepstabilization"])
+        self.settingsWidget.addressLine.setText(plugin_info["address"])
+        self.settingsWidget.sampleNameLine.setText(plugin_info["samplename"])
+        self.settingsWidget.commentLine.setText(plugin_info["comment"])
+        self.settingsWidget.fileNameLine.setText(plugin_info["filename"])
+        self.settingsWidget.logCheckBox.setChecked(False)
+        self.settingsWidget.logWholeBox.setChecked(False)
+
 
     def _setGUIfromSettings(self):
         ##populates GUI with values stored in settings
@@ -373,7 +523,7 @@ class itc503GUI(QObject):
         self._parse_settings_source()
         self._parse_settings_display()
         self._parse_settings_setT()
-
+        self._parse_settings_save()
         try:
             self.settings["sweepstart"] = float(self.settingsWidget.sweepStartEdit.text())
         except ValueError:
@@ -431,6 +581,10 @@ class itc503GUI(QObject):
         return self.settings["sweeppts"]
 
     def loopingIteration(self, iteration):
+        if self.runningFlag:
+            self.run_thread.thread_stop()
+            self._createFileLoop()
+            self.runningFlag = False
         if self.settings["sweeppts"] == 1:
             self.settings["sett"] = self.settings["sweepstart"]
         else:
@@ -455,4 +609,39 @@ class itc503GUI(QObject):
             else:
                 print(datetime.now().strftime("%H:%M:%S.%f") + f" Stabilization period. T={info} K")
             time.sleep(20)
+        if self.settings["log"]:
+            if self.settings["wholetime"]:
+                self.run_thread = thread_with_exception(self._run_check_long)
+                self.run_thread.start()
+            else:
+                self.run_thread = thread_with_exception(self._run_check)
+                self.run_thread.start()
+            self.runningFlag = True
         return [0, f"_{info}K"]
+
+    """def loopingIteration(self, iteration): # test
+        if self.settings["sweeppts"] == 1:
+            self.settings["sett"] = self.settings["sweepstart"]
+        else:
+            self.settings["sett"] = self.settings["sweepstart"] + iteration * (
+                self.settings["sweepend"] - self.settings["sweepstart"]
+            ) / (self.settings["sweeppts"] - 1)
+        # this function is called not from the main thread. Direct addressing of qt elements not from te main thread causes segmentation fault crash. Using a signal-slot interface between different threads should make it work
+        #        self.settingsWidget.setTEdit.setText(f"{self.settings['sett']}")
+        [status, info] = self._setT()
+        if status:
+            return [status, info]
+        #########IRtodo: workaround, make a proper update display
+        tic = time.time()
+        while (time.time() - tic) < self.settings["sweepstabilization"]:
+            try:
+                info = self.itc503.getData()
+            except Exception as e:
+                return [4, {"Error message": f"{e}"}]
+            if abs(info - self.settings["sett"]) > 0.2:
+                print(datetime.now().strftime("%H:%M:%S.%f") + f" wait for T. T={info} K")
+                tic = time.time()
+            else:
+                print(datetime.now().strftime("%H:%M:%S.%f") + f" Stabilization period. T={info} K")
+            time.sleep(20)
+        return [0, f"_{info}K"]"""
