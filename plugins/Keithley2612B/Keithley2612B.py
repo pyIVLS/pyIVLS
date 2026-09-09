bd521ca81a9f24fb8af865a28449870f9e0ddb0c
lumipallero <tuokkolalumi@gmail.com>
2026-09-09 16:41:32 +0300

    trying to see where problem lies

--- plugins/Keithley2612B/Keithley2612B.py
+++ plugins/Keithley2612B/Keithley2612B.py
@@ -977,7 +977,8 @@ class Keithley2612B:
                 self.safewrite(f"{s['source']}.trigger.source.stimulus = 0")
                 # Configure endpulse action to achieve a pulse.
                 self.safewrite(f"{s['source']}.trigger.endpulse.action = {s['source']}.SOURCE_IDLE")
-                self.safewrite(f"{s['source']}.trigger.endpulse.stimulus = trigger.timer[2].EVENT_ID")
+                # self.safewrite(f"{s['source']}.trigger.endpulse.stimulus = trigger.timer[2].EVENT_ID")
+                self.safewrite(f"{s['source']}.trigger.endpulse.stimulus = {s['source']}.trigger.MEASURE_COMPLETE_EVENT_ID")
                 if s["usedrain"]:
                     self.safewrite(f"{s['drain']}.trigger.endpulse.action = {s['drain']}.SOURCE_IDLE")
                     self.safewrite(f"{s['drain']}.trigger.endpulse.stimulus = trigger.timer[2].EVENT_ID")
